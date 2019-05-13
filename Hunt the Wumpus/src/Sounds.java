2d5122da1b904ce4101d0fbde3e98ed57cdbd3b3
s-rsunku <s-rsunku@487M078470.lwsd.k12wa.org>
2019-05-13 09:57:06 -0700

    All Methods implemented. Just need to find sounds.

--- Hunt the Wumpus/src/Sounds.java
+++ Hunt the Wumpus/src/Sounds.java
@@ -7,16 +7,44 @@
  * Revision History
  * 4/18/2019:   Constructor and toString made
  * 4/24/2019:   Studded Methods added
+ * 5/03/2019:   All methods implemented
  */
+
+import java.applet.*;
+import java.net.*;
 public class Sounds
 {
-	private int[] themes;
+	/*
+	 * index 0 is move player, 1 is shoot arrow, 2 is moveWumpus, 3 is trivia pop-up, 4 is win, 5 is lose, 6 is bat, and 7 is pit
+	 */
+	private AudioClip[] currentSounds;
 	/*
 	 * Will read the config file
 	 */
-	public Sounds()
+	public Sounds(int theme) throws MalformedURLException
 	{
-		
+		if (theme==1)
+		{
+			currentSounds[0] = Applet.newAudioClip(new URL("movePlayer1.wav"));
+			currentSounds[1] = Applet.newAudioClip(new URL("shootArrow1.wav"));
+			currentSounds[2] = Applet.newAudioClip(new URL("moveWumpus1.wav"));
+			currentSounds[3] = Applet.newAudioClip(new URL("trivia1.wav"));
+			currentSounds[4] = Applet.newAudioClip(new URL("win1.wav"));
+			currentSounds[5] = Applet.newAudioClip(new URL("lose1.wav"));
+			currentSounds[6] = Applet.newAudioClip(new URL("bat1.wav"));
+			currentSounds[7] = Applet.newAudioClip(new URL("pit1.wav"));
+		}
+		else
+		{
+			currentSounds[0] = Applet.newAudioClip(new URL("movePlayer2.wav"));
+			currentSounds[1] = Applet.newAudioClip(new URL("shootArrow2.wav"));
+			currentSounds[2] = Applet.newAudioClip(new URL("moveWumpus2.wav"));
+			currentSounds[3] = Applet.newAudioClip(new URL("trivia2.wav"));
+			currentSounds[4] = Applet.newAudioClip(new URL("win2.wav"));
+			currentSounds[5] = Applet.newAudioClip(new URL("lose2.wav"));
+			currentSounds[6] = Applet.newAudioClip(new URL("bat2.wav"));
+			currentSounds[7] = Applet.newAudioClip(new URL("pit2.wav"));
+		}
 	}
 	
 	/*
@@ -30,53 +58,51 @@ public class Sounds
 	/*
 	 * @return the possible themes
 	 */
-	
-	public int[] getThemes()
+	public AudioClip[] getSounds()
 	{
-		return themes;
+		return currentSounds;
 	}
 	
 	/*
 	 * rest of the methods are conditions where sounds can be played
 	 */
-	
-	public void movePlayer(int theme)
+	public void movePlayer()
 	{
-		
+		currentSounds[0].play();
 	}
 	
-	public void shootArrow(int theme)
+	public void shootArrow()
 	{
-		
+		currentSounds[1].play();
 	}
 	
-	public void moveWumpus(int theme)
+	public void moveWumpus()
 	{
-		
+		currentSounds[2].play();
 	}
 	
-	public void triviaPopUp(int theme)
+	public void triviaPopUp()
 	{
-		
+		currentSounds[3].play();
 	}
 	
-	public void win(int theme)
+	public void win()
 	{
-		
+		currentSounds[4].play();
 	}
 	
-	public void lose(int theme)
+	public void lose()
 	{
-		
+		currentSounds[5].play();
 	}
 	
-	public void bat(int theme)
+	public void bat()
 	{
-		
+		currentSounds[6].play();
 	}
 	
-	public void pit(int theme)
+	public void pit()
 	{
-		
+		currentSounds[7].play();
 	}
 }
