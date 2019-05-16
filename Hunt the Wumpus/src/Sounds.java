aabf4a7e6fe8f87d7f35e3a405479aa82ec48e7d
s-rsunku <s-rsunku@487M078470.lwsd.k12wa.org>
2019-05-16 10:03:59 -0700

    test

--- Hunt the Wumpus/src/Sounds.java
+++ Hunt the Wumpus/src/Sounds.java
@@ -12,6 +12,7 @@
 
 import java.applet.*;
 import java.net.*;
+import java.io.*;
 public class Sounds
 {
 	/*
@@ -21,29 +22,29 @@ public class Sounds
 	/*
 	 * Will read the config file
 	 */
-	public Sounds(int theme) throws MalformedURLException
+	public Sounds(int theme) throws MalformedURLException, FileNotFoundException
 	{
 		if (theme==1)
 		{
-			currentSounds[0] = Applet.newAudioClip(new URL("https://s-rsunku@bitbucket.org/htw19_p3_artesiancode/htw19_p3_artesiancode.git/config/movePlayer1.wav"));
-			currentSounds[1] = Applet.newAudioClip(new URL("shootArrow1.wav"));
-			currentSounds[2] = Applet.newAudioClip(new URL("moveWumpus1.wav"));
-			currentSounds[3] = Applet.newAudioClip(new URL("trivia1.wav"));
-			currentSounds[4] = Applet.newAudioClip(new URL("win1.wav"));
-			currentSounds[5] = Applet.newAudioClip(new URL("lose1.wav"));
-			currentSounds[6] = Applet.newAudioClip(new URL("bat1.wav"));
-			currentSounds[7] = Applet.newAudioClip(new URL("pit1.wav"));
+			currentSounds[0] = Applet.newAudioClip(new File("movePlayer1.wav").toURI().toURL());
+			currentSounds[1] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\shootArrow1.wav"));
+			currentSounds[2] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\moveWumpus1.wav"));
+			currentSounds[3] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\trivia1.wav"));
+			currentSounds[4] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\win1.wav"));
+			currentSounds[5] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\lose1.wav"));
+			currentSounds[6] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\bat1.wav"));
+			currentSounds[7] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\pit1.wav"));
 		}
 		else
 		{
-			currentSounds[0] = Applet.newAudioClip(new URL("movePlayer2.wav"));
-			currentSounds[1] = Applet.newAudioClip(new URL("shootArrow2.wav"));
-			currentSounds[2] = Applet.newAudioClip(new URL("moveWumpus2.wav"));
-			currentSounds[3] = Applet.newAudioClip(new URL("trivia2.wav"));
-			currentSounds[4] = Applet.newAudioClip(new URL("win2.wav"));
-			currentSounds[5] = Applet.newAudioClip(new URL("lose2.wav"));
-			currentSounds[6] = Applet.newAudioClip(new URL("bat2.wav"));
-			currentSounds[7] = Applet.newAudioClip(new URL("pit2.wav"));
+			currentSounds[0] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\movePlayer2.wav"));
+			currentSounds[1] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\shootArrow2.wav"));
+			currentSounds[2] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\moveWumpus2.wav"));
+			currentSounds[3] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\trivia2.wav"));
+			currentSounds[4] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\win2.wav"));
+			currentSounds[5] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\lose2.wav"));
+			currentSounds[6] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\bat2.wav"));
+			currentSounds[7] = Applet.newAudioClip(new URL("C:\\Users\\s-rsunku\\OneDrive - Lake Washington School District\\APCSA\\Sounds\\pit2.wav"));
 		}
 	}
 	
@@ -56,7 +57,7 @@ public class Sounds
 	}
 	
 	/*
-	 * @return the possible themes
+	 * @return the Sounds
 	 */
 	public AudioClip[] getSounds()
 	{
