380947df20c555eecd822cf7a569ca5a7816cd78
s-rsunku <s-rsunku@487M078470.lwsd.k12wa.org>
2019-05-21 12:06:01 -0700

    changed constructor code

--- Hunt the Wumpus/src/Sounds.java
+++ Hunt the Wumpus/src/Sounds.java
@@ -7,16 +7,45 @@
  * Revision History
  * 4/18/2019:   Constructor and toString made
  * 4/24/2019:   Studded Methods added
+ * 5/03/2019:   All methods implemented
  */
+
+import java.applet.*;
+import java.net.*;
+import java.io.*;
 public class Sounds
 {
-	private int[] themes;
+	/*
+	 * index 0 is move player, 1 is shoot arrow, 2 is moveWumpus, 3 is trivia pop-up, 4 is win, 5 is lose, 6 is bat, and 7 is pit
+	 */
+	private AudioClip[] currentSounds;
 	/*
 	 * Will read the config file
 	 */
-	public Sounds()
+	public Sounds(int theme) throws MalformedURLException, FileNotFoundException
 	{
-		
+		if (theme==1)
+		{
+			currentSounds[0] = Applet.newAudioClip(new File("input/movePlayer1.wav").toURI().toURL());
+			currentSounds[1] = Applet.newAudioClip(new File("input/shootArrow1.wav").toURI().toURL());
+			currentSounds[2] = Applet.newAudioClip(new File("input/moveWumpus1.wav").toURI().toURL());
+			currentSounds[3] = Applet.newAudioClip(new File("input/trivia1.wav").toURI().toURL());
+			currentSounds[4] = Applet.newAudioClip(new File("input/win1.wav").toURI().toURL());
+			currentSounds[5] = Applet.newAudioClip(new File("input/lose1.wav").toURI().toURL());
+			currentSounds[6] = Applet.newAudioClip(new File("input/bat1.wav").toURI().toURL());
+			currentSounds[7] = Applet.newAudioClip(new File("input/pit1.wav").toURI().toURL());
+		}
+		else
+		{
+			currentSounds[0] = Applet.newAudioClip(new File("input/movePlayer2.wav").toURI().toURL());
+			currentSounds[1] = Applet.newAudioClip(new File("input/shootArrow2.wav").toURI().toURL());
+			currentSounds[2] = Applet.newAudioClip(new File("input/moveWumpus2.wav").toURI().toURL());
+			currentSounds[3] = Applet.newAudioClip(new File("input/trivia2.wav").toURI().toURL());
+			currentSounds[4] = Applet.newAudioClip(new File("input/win2.wav").toURI().toURL());
+			currentSounds[5] = Applet.newAudioClip(new File("input/lose2.wav").toURI().toURL());
+			currentSounds[6] = Applet.newAudioClip(new File("input/bat2.wav").toURI().toURL());
+			currentSounds[7] = Applet.newAudioClip(new File("input/pit2.wav").toURI().toURL());
+		}
 	}
 	
 	/*
@@ -28,55 +57,53 @@ public class Sounds
 	}
 	
 	/*
-	 * @return the possible themes
+	 * @return the Sounds
 	 */
-	
-	public int[] getThemes()
+	public AudioClip[] getSounds()
 	{
-		return themes;
+		return currentSounds;
 	}
 	
 	/*
 	 * rest of the methods are conditions where sounds can be played
 	 */
-	
-	public void movePlayer(int theme)
+	public void movePlayer()
 	{
-		
+		currentSounds[0].play();
 	}
 	
-	public void shootArrow(int theme)
+	public void shootArrow()
 	{
-		
+		currentSounds[1].play();
 	}
 	
-	public void moveWumpus(int theme)
+	public void moveWumpus()
 	{
-		
+		currentSounds[2].play();
 	}
 	
-	public void triviaPopUp(int theme)
+	public void triviaPopUp()
 	{
-		
+		currentSounds[3].play();
 	}
 	
-	public void win(int theme)
+	public void win()
 	{
-		
+		currentSounds[4].play();
 	}
 	
-	public void lose(int theme)
+	public void lose()
 	{
-		
+		currentSounds[5].play();
 	}
 	
-	public void bat(int theme)
+	public void bat()
 	{
-		
+		currentSounds[6].play();
 	}
 	
-	public void pit(int theme)
+	public void pit()
 	{
-		
+		currentSounds[7].play();
 	}
 }
