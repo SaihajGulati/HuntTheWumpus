642f4c2d217929a6c89f0ddb13522fe6e02787c4
s-jvenable <s-jvenable@10.0.0.55>
2019-05-29 08:58:37 -0700

    k

--- Hunt the Wumpus/src/GameControl.java
+++ Hunt the Wumpus/src/GameControl.java
@@ -234,106 +234,7 @@ public class GameControl
 			HighScore.updateScoreBoard(endScore, Name, CaveName);
 			//GI.displayHighscore(HighScore.getHighScores());
 
-	}
-	/**
-	 *
-	 * @return whether or not you can move there
-	 */
-	public static boolean movePossible() 
-	{	
-		//locations.<method to return room number>
-		//if there is a door leading there and he is adjacent to it then return true
-		return false;
-	}
-	
-	/**
-	 * 
-	 * @return the map fully created if player decides to play the game
-	 */
-	public static ArrayList<Integer> createMap()
-	{
-		//if player clicks 'Play Game'
-		//NOTE : Integer is a placeholder and will be replaced with a cave type
-		//cave method replaces 'new ArrayList<Integer>();' to grab the generated cave
-		ArrayList<Integer> map = new ArrayList<Integer>();
-		return map;
-	}
-	
-	/**
-	 * 
-	 * @return the location of the player as an integer index in the arrayList
-	 */
-	public static int playerLocation() 
-	{
-		//get this from a GameLocation method and return the index for the player and the GI 
-		return 0;
-	}
-	
-	/**
-	 * 
-	 * @param 'words' is given by trivia and given to the player when the GI needs it
-	 * @return the random questions that are given by trivia
-	 */
-	public static String[] getQuestions(String[] words)
-	{
-		// simple passing method so trivia doesn't have to extend
-		return words;
-	}
-
-	
-	/**
-	 * 
-	 * @param answers are given from GI and passed in order to check with trivia
-	 * @return the answers as an array and given to trivia
-	 */
-	public static String[] giveAnswer(String[] answers)
-	{
-		//do not add anything else for now this should be a simple pass
-		return answers;
-	}
-	
-	/**
-	 * 
-	 * @param items given from player in order to show GI and gameScore
-	 * @return the item list to give to GI and gameScore
-	 */
-	public static int[] itemList(int[] items)
-	{
-		//another simple array carry over
-		return items;
-	}
-	
-	/**
-	 * 
-	 * @return the index of the cave they want to shoot for GameLocations
-	 */
-	public static int shootArrow()
-	{
-		//Do not include 'shot' boolean as a parameter, do it inside main code
-		//code for getting the index (using same rules as the movingPlayer)
-		return 0;
-	}
-	
-	/**
-	 * 
-	 * @return boolean of whether or not Wumpus was shot
-	 */
-	public static boolean shotWumpus()
-	{
-		//return true if the room shot has the Wumpus
-		//I need the return value from GameLocations after giving it 'shootArrow' method
-		return false;
-	}
-	
-	/**
-	 * 
-	 * @return the total high score for the GI
-	 */
-	public static int getScore()
-	{
-		//input the player method that calculates the high score
-		return 0;
-	}
+	}	
 	/**
 	 * prints what hazards there are in adjacent rooms
 	 */
@@ -386,16 +287,6 @@ public class GameControl
 			word += (array[0] + " " + andor + " " + array[1] + ".");
 		}
 		return word;
-	}
-	
-	/**
-	 * 
-	 * @return whether or not the Wumpus is within 2 rooms of the player (for ease of use to the trivia class)
-	 */
-	public static boolean WumpusIsnear() 
-	{
-		//code here that asks game location to compare the the distance between the player and wumpus class
-		return false;
 	}
 	
 	/**
