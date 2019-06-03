c1e1efdbc6d9ca299eb08b2277b4bcb70e80d369
s-dapopa <s-dapopa@192.168.10.117>
2019-06-02 19:45:45 -0700

    almost done

--- Hunt the Wumpus/src/GraphicalInterface.java
+++ Hunt the Wumpus/src/GraphicalInterface.java
@@ -1320,7 +1320,7 @@ public void  setTheme()
 		waiting = false;
 	}
 	
-	if(button(0.5, 0.21 , 0.15 , 0.055, "wack",false))
+	if(button(0.5, 0.21 , 0.15 , 0.055, "weird",false))
 	{
 		changeToScary(false);
 		scaryTheme = false;
